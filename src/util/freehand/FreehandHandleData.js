a17b07f7ff5700e2eba0797cf77ecfb2d8c11d90
Tomáš Taro <tomasko126@gmail.com>
2023-02-24 17:30:35 +0100

    isRefinementPoint -> isCommonPoint

--- src/util/freehand/FreehandHandleData.js
+++ src/util/freehand/FreehandHandleData.js
@@ -14,19 +14,19 @@ export default class FreehandHandleData {
    * Constructs a single handle for the freehand tool
    *
    * @param {Object} position - The position of the handle.
-   * @param {boolean} isRefinementPoint - whether the point is dedicated for grid refinement
+   * @param {boolean} isCommonPoint - whether the point is main 'common' or 'refinement' one dedicated for grid refinement
    * @param {boolean} highlight - whether the handle should be rendered as the highlighted color.
    * @param {boolean} active - whether the handle is active.
    */
   constructor(
     position,
-    isRefinementPoint = false,
+    isCommonPoint = false,
     highlight = true,
     active = true
   ) {
     this.x = position.x;
     this.y = position.y;
-    this.isRefinementPoint = isRefinementPoint;
+    this.isCommonPoint = isCommonPoint;
     this.highlight = highlight;
     this.active = active;
     this.lines = [];
